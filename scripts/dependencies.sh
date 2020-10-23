c227ea4666cdc865e1ddbc2b88dbd0b27f8eb610
hollyb18 <hollyb18@gmail.com>
2020-10-23 09:13:31 +0100

    changed ansible installation

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -2,7 +2,6 @@
 
 sudo apt-get update
 sudo apt install python3-pip
-python -m pip install --user ansible
 sudo apt-get update
 sudo apt install ansible
 ansible-galaxy collection install community.general
