bc0c29bfd073ebf02cc428314efc87a0aeca36be
hollyb18 <hollyb18@gmail.com>
2020-10-23 09:58:05 +0100

    corrected typo for service-3 in push.sh

--- scripts/push.sh
+++ scripts/push.sh
@@ -7,7 +7,7 @@ sudo docker tag service-2 localhost:8082/service-2:"$(git rev-parse --short HEAD
 sudo docker push localhost:8082/service-2:"$(git rev-parse --short HEAD)"
 sudo docker rmi localhost:8082/service-2:"$(git rev-parse --short HEAD)"
 sudo docker tag service-3 localhost:8082/service-3:"$(git rev-parse --short HEAD)"
-sudo docker push localhost:8082/service-3:"$(git rev-aprse --short HEAD)"
+sudo docker push localhost:8082/service-3:"$(git rev-parse --short HEAD)"
 sudo docker rmi localhost:8082/service-3:"$(git rev-parse --short HEAD)"
 sudo docker tag service-4 localhost:8082/service-4:"$(git rev-parse --short HEAD)"
 sudo docker push localhost:8082/service-4:"$(git rev-parse --short HEAD)"
