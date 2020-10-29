aca5fd756553ea4deb41581ed4674283d3a5edd8
hollyb18 <hollybate18@gmail.com>
2020-10-29 11:18:10 +0000

    corrected --cov ./application/ line

--- scripts/pytest.sh
+++ scripts/pytest.sh
@@ -4,14 +4,14 @@ python3 -m venv venv
 . ./venv/bin/activate
 cd /home/student/nbs-mentored-project/roles/service-1/files/
 pip install -r requirements.txt
-pytest --cov
+pytest --cov ./application/
 cd /home/student/nbs-mentored-project/roles/service-2/files/
 pip install -r requirements.txt
-pytest --cov
+pytest --cov ./application/
 cd /home/student/nbs-mentored-project/roles/service-3/files/
 pip install -r requirements.txt
-pytest --cov
+pytest --cov ./application/
 cd /hom/student/nbs-mentored-project/roles/service-4/files/
 pip install -r requirements.txt
-pytest --cov
+pytest --cov ./application/
 exit
