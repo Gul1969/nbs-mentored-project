c14c0fa0110a1db2893fa9429c1fc645c70a89ec
hollyb18 <hollybate18@gmail.com>
2020-10-29 11:05:03 +0000

    added coverage testing with --cov command in pytest

--- scripts/pytest.sh
+++ scripts/pytest.sh
@@ -4,14 +4,14 @@ python3 -m venv venv
 . ./venv/bin/activate
 cd /home/student/nbs-mentored-project/roles/service-1/files/
 pip install -r requirements.txt
-pytest
+pytest --cov
 cd /home/student/nbs-mentored-project/roles/service-2/files/
 pip install -r requirements.txt
-pytest
+pytest --cov
 cd /home/student/nbs-mentored-project/roles/service-3/files/
 pip install -r requirements.txt
-pytest
+pytest --cov
 cd /hom/student/nbs-mentored-project/roles/service-4/files/
 pip install -r requirements.txt
-pytest
+pytest --cov
 exit
