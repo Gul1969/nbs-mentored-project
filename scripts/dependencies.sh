864b36db23adcaec08f1137a9dca61df14496144
hollyb18 <hollybate18@gmail.com>
2020-10-30 12:41:52 +0000

    editing dependencies.sh

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -1,11 +1,12 @@
 #! /bin/bash
 
-sudo apt remove ansible
+sudo apt remove ansible -y
 sudo apt-get update
 sudo apt install python3-pip
 sudo apt install python-pip
 sudo apt install python3-venv
 sudo apt-get update
+python -m pip install --user ansible
 sudo -H python -m pip install ansible
 sudo apt install ansible
 ansible-galaxy collection install community.general
