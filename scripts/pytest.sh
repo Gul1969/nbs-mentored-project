7a904d0c340401d0f95331d61b6adc0974bc5110
hollyb18 <hollybate18@gmail.com>
2020-10-30 11:33:25 +0000

    changed permissions

--- scripts/pytest.sh
+++ scripts/pytest.sh
@@ -2,7 +2,6 @@
 
 python3 -m venv venv
 . ./venv/bin/activate
-sudo su - jenkins
 cd /home/student/nbs-mentored-project/roles/service-1/files/
 pip install -r requirements.txt
 pytest --cov ./application/
@@ -15,4 +14,4 @@ pytest --cov ./application/
 cd /home/student/nbs-mentored-project/roles/service-4/files/
 pip install -r requirements.txt
 pytest --cov ./application/
-exit
+deactivate
