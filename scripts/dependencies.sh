37a6959e2a41f63091a9c62198e9a1224261d26b
hollyb18 <hollyb18@gmail.com>
2020-11-02 12:57:09 +0000

    added pom.xml file for artefact scanning

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -2,11 +2,12 @@
 
 sudo apt remove ansible -y
 sudo apt-get update
-sudo apt install python3-pip
-sudo apt install python-pip
-sudo apt install python3-venv
+sudo apt install python3-pip -y
+sudo apt install python-pip -y
+sudo apt install python3-venv -y
+sudo apt install maven default-jdk -y
 sudo apt-get update
 python -m pip install --user ansible
 sudo -H python -m pip install ansible
-sudo apt install ansible
+sudo apt install ansible 
 ansible-galaxy collection install community.general
